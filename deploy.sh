7f8b82556f2b96edf71a89d1520dc349a829e8ef
YoungsunLi <lsun@live.cn>
2019-04-17 21:25:26 +0800

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd dist
+
+# 如果是发布到自定义域名(待启用)
+# echo 'lsun.net' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 发布到 https://<USERNAME>.github.io
+git push -f git@github.com:YoungsunLi/youngsunli.github.io.git master
+
+cd -
